f31325e7b93a8d3d27030f1ec1bda94534507a83
ARUQUIPA FLORES ALAN NAHUEL <aaruquipaflores639@alumno.unlam.edu.ar>
2025-05-27 22:25:45 -0300

    init

---
+++ src/test/java/com/ia/azure/recognition/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ia.azure.recognition.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
